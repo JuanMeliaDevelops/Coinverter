e3f924a314d31a6ee72a8aca9c0ce9a1a19072c0
juanimelia <juanimeliausa@gmail.com>
2023-08-15 16:52:19 -0300

    numberInput maxLenght 10

--- js/functions/numberInput.js
+++ js/functions/numberInput.js
@@ -1,12 +1,12 @@
 //      Evento solo numeros
 
 function validaNumeros(event) {
-    if (event.charCode >= 48 && event.charCode <= 57) {
+
+    if (event.charCode >= 48 && event.charCode <= 57 && moneda1.value.length < 10) {
       return true;
     }
-    return false;
+    else {
+      return false;
+    }
   }
-  
-  
-  
-  
+
