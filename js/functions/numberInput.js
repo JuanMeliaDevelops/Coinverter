c360cc0d1cb770c7fea5fed5c88ca6778c3e9e48
juanimelia <juanimeliausa@gmail.com>
2023-10-17 11:45:09 -0300

    Aceptar numeros decimales (.) en numberInput.js

--- js/functions/numberInput.js
+++ js/functions/numberInput.js
@@ -2,11 +2,11 @@
 
 function validaNumeros(event) {
 
-    if (event.charCode >= 48 && event.charCode <= 57 && moneda1.value.length < 10) {
-      return true;
-    }
-    else {
-      return false;
-    }
+  if ((event.charCode >= 48 && event.charCode <= 57 && moneda1.value.length < 10) || event.key === '.') {
+    return true;
   }
+  else {
+    return false;
+  }
+}
 
